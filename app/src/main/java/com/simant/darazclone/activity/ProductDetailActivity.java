bb48c64c334b78042d3959c5554b6f79f6a890ee
Simant Dhakal <Simant.dhakal@gmail.com>
2020-01-04 22:58:30 +0545

    Minor error resolve. Added Login/Signup.

---
+++ app/src/main/java/com/simant/darazclone/activity/ProductDetailActivity.java
@@ -0,0 +1,26 @@
+package com.simant.darazclone.activity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.simant.darazclone.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
---
+++ app/src/main/java/com/simant/darazclone/activity/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.simant.darazclone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.simant.darazclone.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
